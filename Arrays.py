fa7fab0ca317015c6f49b23afd2999c35d7d5ec5
Timothy <taoladoja011203@gmail.com>
2024-09-14 12:10:36 +0100

    Data structure

---
+++ Arrays.py
@@ -0,0 +1,17 @@
+new_List = [1, 2, 3]
+result = new_List[0]
+
+if 1 in new_List:
+    print(True)
+
+
+for n in new_List:
+    if n == 1:
+        print(True)
+    break
+
+numbers =[]
+len(numbers)
+numbers.append(2)
+numbers.append(200)
+
