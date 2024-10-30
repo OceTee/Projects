8772defb28c96ba887dbf13f9335d88c0effeebb
OceTee <taoladoja011203@gmail.com>
2024-10-30 09:30:36 -0700

    update

---
+++ Mini-games/Dependencies_Checklist.py
@@ -0,0 +1,14 @@
+"""
+Checklist for the Dependencies used for the Minigames
+"""
+
+import pygame
+import tkinter as tk
+import random
+import math
+import time
+import sys
+from collections import deque, Counter
+import json
+import os
+import itertools
